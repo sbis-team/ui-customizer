347ba3b734c26e451c4c1375cc4f6d3e8bf30dfb
IgorNovozhilov <maildrakon@gmail.com>
2016-12-07 23:19:07 +0300

    Обновление v1.0.0
    Сборка от: 07.12.2016 23:19:01
    
    #### Новые возможности
    
    * Плагин успешно переехал на <a href="https://github.com/sbis-team/ui-customizer" target="_blank">GitHub</a>.<br>Это первый публичный релиз, теперь плагин доступен из дома без VPN, и сотрудникам у которых нет доступа на корпоративный GitLab

---
+++ sbis-ui-customizer.meta.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name          SBIS UI-Customizer v1.0.0
+// @namespace     SBIS
+// @version       1.0.0
+// @date          2016-12-07T23:19:01
+// @build         46
+// @author        Новожилов И. А.
+// @description   Пользовательская настройка web интерфейса сайтов SBIS
+// @homepage      https://github.com/sbis-team/ui-customizer
+// @icon          data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAAASFBMVEUAAAAzMzMyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIzMzMxMTExMTExMTEyMjIyMjIyMjIxMTEzMzM1NTU0NDQyMjILZXhtAAAAF3RSTlMAgIh73tmNbxuddvjv7eIfE6IjvrwmJxF/neAAAABwSURBVBjTZY9ZDoAgEEMLjCAuCG7c/6aC1YTofDQvL5kmBeyogTOE40HkPgPGWkMsaUqqcg8One4GCnPj1GOZKYjTgpWCWF/EURB/pcioYtsUkRmD9yE2wnnAu49IrYiSkrQvUPv+lv7H6br5EFmJF6QjBzKAskwHAAAAAElFTkSuQmCC
+// @icon64        data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAMAAACdt4HsAAAAbFBMVEUAAAAyMjIsLCwyMjIyMjIyMjIyMjIyMjIyMjIyMjIzMzMyMjIzMzMyMjI1NTUzMzMxMTEzMzMxMTEyMjIyMjIxMTEyMjIzMzMyMjIxMTEvLy8AAAAzMzMxMTErKysyMjIyMjIyMjI0NDQzMzNV1+DKAAAAI3RSTlMA+AfjFc/frI5wWDUwkwnMyW3TsZiIPDlQ6BsBLBoF84pMHXeJqhAAAAGcSURBVFjD3ZeNboIwFIVpBRQBAcX/Cdv6/u+4XrpxYeOmOyEhbjfG0s/jSSmlJw1c6aps27LSwah0EilVJL+gt8h0FT2G9H3v6P7mo5r+7xyaAd33VHtoZfqqWJowTTy0tL11GB5sU7I0Ylp4aGt7YRA8bNOyVDka2kZ5qLHVtwD9bwYr+nikotZwyQay9r4Zws1dMJC1jjEVDETtha5Unmqd5oquL7LBdkLbrGhWMifIuo6WDHT388tYS6aKmKPkuxUMprVrvm2qnN4VwWBaG9vvM8OT7caSAQ06/a6lcWiGOxqlZGCmtGauwQq4hUktTUzOsPZM4g+tezTZF0v9j5G1hrRucVyzT3alUTWCgaB1S7k+73an2lC9SUtZ0r6aUR2ll0nWHsdMMBC0czcUhrFSsd+AtU+4Kz+rASUAaoCkBZIAiAGQFkgCCFogARADIC2ABEAMgK0W2MBnGyx6C0BaAAmAGABpASQAYACkBZAAi28onAB/fFdewEA4SIG0sL0DH/twmgwPngAVjrMAFQ/fONVJoRQf/wH6AaEt37xRS8ZKAAAAAElFTkSuQmCC
+// @supportURL    https://github.com/sbis-team/ui-customizer/issues
+// @include       https://inside.tensor.ru/*
+// @exclude       https://inside.tensor.ru/obj/*
+// @exclude       https://inside.tensor.ru/shared/*
+// @exclude       https://inside.tensor.ru/docview/*
+// @exclude       https://inside.tensor.ru/auth/*
+// @include       https://fix-inside.tensor.ru/*
+// @exclude       https://fix-inside.tensor.ru/obj/*
+// @exclude       https://fix-inside.tensor.ru/shared/*
+// @exclude       https://fix-inside.tensor.ru/docview/*
+// @exclude       https://fix-inside.tensor.ru/auth/*
+// @include       https://rc-inside.tensor.ru/*
+// @exclude       https://rc-inside.tensor.ru/obj/*
+// @exclude       https://rc-inside.tensor.ru/shared/*
+// @exclude       https://rc-inside.tensor.ru/docview/*
+// @exclude       https://rc-inside.tensor.ru/auth/*
+// @include       https://test-inside.tensor.ru/*
+// @exclude       https://test-inside.tensor.ru/obj/*
+// @exclude       https://test-inside.tensor.ru/shared/*
+// @exclude       https://test-inside.tensor.ru/docview/*
+// @exclude       https://test-inside.tensor.ru/auth/*
+// @include       https://pre-test-inside.tensor.ru/*
+// @exclude       https://pre-test-inside.tensor.ru/obj/*
+// @exclude       https://pre-test-inside.tensor.ru/shared/*
+// @exclude       https://pre-test-inside.tensor.ru/docview/*
+// @exclude       https://pre-test-inside.tensor.ru/auth/*
+// @include       https://dev-inside.tensor.ru/*
+// @exclude       https://dev-inside.tensor.ru/obj/*
+// @exclude       https://dev-inside.tensor.ru/shared/*
+// @exclude       https://dev-inside.tensor.ru/docview/*
+// @exclude       https://dev-inside.tensor.ru/auth/*
+// @run-at        document-start
+// @grant         unsafeWindow
+// @grant         GM_setClipboard
+// @noframes
+// ==/UserScript==
