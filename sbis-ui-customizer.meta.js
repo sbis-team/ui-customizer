67bf73c2f4590dcfea922f9c2edcfc9cafc1d1eb
ia.novozhilov <ia.novozhilov@tensor.ru>
2016-12-15 17:43:48 +0300

    Обновление v1.1.0.rc7
    Сборка от: 15.12.2016 17:43:43
    
    #### Новые возможности
    
    * Новая опция 'Главная страница > Новости > Скрыть вложения под спойлер'

--- sbis-ui-customizer.meta.js
+++ sbis-ui-customizer.meta.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name          SBIS UI-Customizer v1.1.0.rc6
+// @name          SBIS UI-Customizer v1.1.0.rc7
 // @namespace     SBIS
-// @version       1.1.0.rc6
-// @date          15.12.2016 17:42:59
+// @version       1.1.0.rc7
+// @date          15.12.2016 17:43:43
 // @author        Новожилов И. А.
 // @description   Пользовательская настройка web интерфейса сайтов SBIS
 // @homepage      https://github.com/sbis-team/ui-customizer
