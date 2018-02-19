01e90f172b3989f3e3ccd995e4f0030a4acb4624
ia.novozhilov <ia.novozhilov@tensor.ru>
2018-02-19 13:29:22 +0300

    Обновление v1.3.10.rc1 Небольшие изменения  - Открытие диалога настроек через HotKeys: ctrl+shift+U и ctrl+alt+U  - Опция скрытия переключателя Мини/Макси в шапке сайта Исправленные ошибки  - Правки стилей после обновления rc-3.18.10  - Убрана иконка настроек в шапке сайта (глючит шапка)

--- sbis-ui-customizer.meta.js
+++ sbis-ui-customizer.meta.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name          SBIS UI-Customizer v1.3.9.rc1
+// @name          SBIS UI-Customizer v1.3.10.rc1
 // @namespace     SBIS
-// @version       1.3.9.rc1
-// @date          29.01.2018 18:08:29
+// @version       1.3.10.rc1
+// @date          19.02.2018 13:29:17
 // @author        Новожилов И. А.
 // @description   Пользовательская настройка web интерфейса сайтов SBIS
 // @homepage      https://github.com/sbis-team/ui-customizer
