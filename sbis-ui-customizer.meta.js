09a9bad99b2979a036abc091d28bfd1c4b1aa91a
ia.novozhilov <ia.novozhilov@tensor.ru>
2019-06-21 10:06:48 +0300

    Обновление v1.4.8.rc1 Исправленные ошибки  - Исправлено копирование описания задачи для коммита

--- sbis-ui-customizer.meta.js
+++ sbis-ui-customizer.meta.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name          SBIS UI-Customizer v1.4.7.rc1
+// @name          SBIS UI-Customizer v1.4.8.rc1
 // @namespace     SBIS
-// @version       1.4.7.rc1
-// @date          21.03.2019 19:11:11
+// @version       1.4.8.rc1
+// @date          21.06.2019 10:06:42
 // @author        Новожилов И. А.
 // @description   Пользовательская настройка web интерфейса сайтов SBIS
 // @homepage      https://github.com/sbis-team/ui-customizer
