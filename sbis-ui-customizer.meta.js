47baac59f8e69e59ac7c4c24636d7cd8593b22ec
ia.novozhilov <ia.novozhilov@tensor.ru>
2019-09-27 10:31:55 +0300

    Обновление v1.4.10 Исправленные ошибки  - Скрыватель кнопки помощи

--- sbis-ui-customizer.meta.js
+++ sbis-ui-customizer.meta.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name          SBIS UI-Customizer v1.4.9
+// @name          SBIS UI-Customizer v1.4.10
 // @namespace     SBIS
-// @version       1.4.9
-// @date          20.08.2019 12:34:58
+// @version       1.4.10
+// @date          27.09.2019 10:31:49
 // @author        Новожилов И. А.
 // @description   Пользовательская настройка web интерфейса сайтов SBIS
 // @homepage      https://github.com/sbis-team/ui-customizer
