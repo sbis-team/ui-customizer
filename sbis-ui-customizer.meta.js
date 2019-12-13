776c6822b523c7e3d04dba9be3146679082102f8
IgorNovozhilov <maildrakon@gmail.com>
2019-12-13 21:53:15 +0300

    Обновление v1.4.12.rc1 Небольшие изменения  - Новогоднее обновление

--- sbis-ui-customizer.meta.js
+++ sbis-ui-customizer.meta.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name          SBIS UI-Customizer v1.4.11.rc1
+// @name          SBIS UI-Customizer v1.4.12.rc1
 // @namespace     SBIS
-// @version       1.4.11.rc1
-// @date          04.10.2019 10:02:44
+// @version       1.4.12.rc1
+// @date          13.12.2019 21:53:06
 // @author        Новожилов И. А.
 // @description   Пользовательская настройка web интерфейса сайтов SBIS
 // @homepage      https://github.com/sbis-team/ui-customizer
